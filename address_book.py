bd08c24117eb9cdc2c007574a0f8025620a79dcc
Renzo Arias <rariasbarrero@gmail.com>
2022-07-20 04:44:40 -0500

    commit inicial

---
+++ address_book.py
@@ -0,0 +1,35 @@
+import csv
+from datetime import date
+from os import path
+
+print("ingrese su nombre:")
+name = input()
+
+print("ingrese su dirección:")
+address = input()
+
+print("ingrese su teléfono:")
+num = input()
+
+fecha_actual = date.today()
+
+identificador = format(id(name), 'x')
+
+reg_date = fecha_actual.strftime('%d/%m/%Y')
+lista = [ ]
+lista.append(name)
+lista.append(address)
+lista.append(num)
+lista.append(identificador)
+lista.append(reg_date)
+
+directorio = path.dirname(path.abspath(__file__))
+archivo = path.join(directorio, "addresses.csv")
+with open(archivo, "a") as cvfile:
+    writer = csv.writer(cvfile)
+    writer.writerow(lista)
+
+print(f"Nombre: {name}")
+print(f"Dirección: {num}")
+print(f"Fecha de registro: {fecha_actual}")
+print(f"ID: {identificador}")   
