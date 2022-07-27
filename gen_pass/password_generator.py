1ff92d4aff6ff6a10668aca8d8cb27511b8f4e4b
Renzo Arias <rariasbarrero@gmail.com>
2022-07-27 05:57:22 -0500

    control de salida funcion_gen/line 56

--- gen_pass/password_generator.py
+++ gen_pass/password_generator.py
@@ -1,56 +1,6 @@
-import string
-import random
-
-mayu = list(string.ascii_uppercase)
-minus = list(string.ascii_lowercase)
-numeros = list(string.digits)
-caracteres_especiales = list("+-%$&#")
-caracteres = list(string.ascii_uppercase + string.ascii_lowercase + string.digits + "+-%$&#")
-#ascii_letters considera letras mayusculas y minusculas
-#string.digits considera numeros '0123456789'
-
-def generador_pass():
-    q1 = input("¿Desea ingresar la longitud de la contraseña? s/n: ")
-    
-    if q1 == "s":
-        length = int(input("ingrese la longitud: "))
-    else:
-        length = 16
-        
-    mayu2 = input("¿Desea que la contraseña contenga mayusculas? s/n: ")
-    minus2 = input("¿Desea que la contraseña contenga minusculas? s/n:  ")
-    num2 = input("¿Desea que la contraseña contenga numeros? s/n:")
-    cespeciales = input("¿Desea que la contraseña contenga caracteres especiales? s/n: ")
-    
-    
-    password = []
-        
-    if mayu2 == "s":
-        random.shuffle(mayu)
-        for i in range(length):
-            password.append(random.choice(mayu))
-            
-    if minus2 == "s":
-        random.shuffle(minus)
-        for i in range(length):
-            password.append(random.choice(minus))
-
-    if num2 == "s":
-        random.shuffle(numeros)
-        for i in range(length):
-            password.append(random.choice(numeros))
-            
-    if cespeciales == "s":
-        random.shuffle(caracteres_especiales)
-        for i in range(length):
-            password.append(random.choice(caracteres_especiales))
-    
-    
-
-    random.shuffle(password)
-
-    print("".join(password))
+from funcion_gen import generador_pass
 
 
-
+print("=======Generador De Contraseñas========")
+print()
 generador_pass()
