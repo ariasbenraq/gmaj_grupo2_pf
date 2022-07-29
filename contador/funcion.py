9e0f38ec4e08cc5e3e8bf81182c02643fa336098
Renzo Arias <rariasbarrero@gmail.com>
2022-07-29 12:27:12 -0500

    çorrecciones lines 8 y 16 if else anidado

--- contador/funcion.py
+++ contador/funcion.py
@@ -3,9 +3,5 @@ def filtro(x):
     if x == "adios":
         return "adios"
 
-# frase = input()
-# frase = frase.split()
 
-# fili = list(filter(filtro, frase))
 
-# print(fili)
--- contador/word_counter.py
+++ contador/word_counter.py
@@ -4,52 +4,32 @@ import time
 from funcion import filtro
 from os import path
 
-
-
-
-print("Ingresa una frase: ")
-frase = input()
-word = frase.split()
-count = len(word)
-ciclo = 1
-word_trick = ["adios"]
-
-directorio = path.dirname(path.abspath(__file__)) 
-archivo = path.join(directorio, "phrases.txt")
-with open("contador\phrases.txt", "a") as archivo:
-    archivo.write(frase + "\n")
-
-print(f"Cantidad de frases: {ciclo}")
-print(f"cantidad de palabras: {count}")
-
-frase_1 = list(filter(filtro, word))
-frase_1 ="".join(frase_1)
+ciclo = 0
+davar = 0
+frase_1 = 0
 
 while frase_1 != "adios":
     print()
     print("Ingresa una nueva frase: ")
     frase = input()
     word = frase.split()
-    count = len(word)
-    ciclo = ciclo + 1
-    directorio = path.dirname(path.abspath(__file__)) 
-    archivo = path.join(directorio, "phrases.txt")
-    with open("contador\phrases.txt", "a") as archivo:
-        archivo.write(frase + "\n")
+    if not list(filter(filtro, word)):
+        count = len(word)
+        davar = davar + count
+        ciclo = ciclo + 1
+        directorio = path.dirname(path.abspath(__file__)) 
+        archivo = path.join(directorio, "phrases.txt")
+        with open("contador\phrases.txt", "a") as archivo:
+            archivo.write(frase + "\n")
 
-    print(f"Cantidad de frases: {ciclo}")
-    print(f"cantidad de palabras: {count}")
-    print()
+        print(f"Cantidad de frases: {ciclo}")
+        print(f"cantidad de palabras: {davar}")
+    else:
+        print()
+        print("Muchas gracias por compartir estas frases")
+        time.sleep(1)
+        print("Hasta pronto")
     
     frase_1 = list(filter(filtro, word))
-    
-    
     frase_1 ="".join(frase_1)
     
-
-   
-
-else:
-    print("Muchas gracias por compartir estas frases")
-    time.sleep(1)
-    print("Hasta pronto")
